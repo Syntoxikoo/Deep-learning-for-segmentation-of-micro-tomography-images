6337aab448eb8c04211ac9cf214d3a24a574f42d
Eliot Colas <eliot.colas29120@gmail.com>
2025-12-01 12:24:39 +0100

    change to training settings

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,5 @@
-from .custom_loss import WeightedCrossEntropyLoss, WeightedCrossEntropyLossV2
-from .dataset_loader import UnetDataset, TOMODataset
+from .custom_loss import DiceLoss, WeightedCrossEntropyLoss, WeightedCrossEntropyLossV2
+from .dataset_loader import TOMODataset, UnetDataset
 from .tools import (
     PairTransform,
     get_device,
@@ -10,6 +10,7 @@ from .tools import (
 from .weights_map_unet_paper import compute_weight_map, compute_weight_mapV2
 
 __all__ = [
+    "DiceLoss",
     "WeightedCrossEntropyLoss",
     "WeightedCrossEntropyLossV2",
     "UnetDataset",
