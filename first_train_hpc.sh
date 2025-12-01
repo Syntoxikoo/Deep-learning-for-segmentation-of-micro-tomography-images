8a5d567d01d253072973df632d6b7507be3a3d30
Syntoxicoo <eliot.colas29120@gmail.com>
2025-12-01 18:21:32 +0100

    small clean

--- first_train_hpc.sh
+++ first_train_hpc.sh
@@ -26,21 +26,12 @@ module load ffmpeg
 
 mkdir -p logs
 
-# LOCAL_DATA_DIR="/scratch/$LSB_JOBID/datas"
-# NETWORK_DATA_DIR="$PWD/datas"
-
-# echo "Copying data to local scratch ($LOCAL_DATA_DIR)..."
-# mkdir -p "$LOCAL_DATA_DIR"
-# rsync -a "$NETWORK_DATA_DIR/Original Images" "$LOCAL_DATA_DIR/"
-# rsync -a "$NETWORK_DATA_DIR/Original Masks" "$LOCAL_DATA_DIR/"
-# echo "Data copy finished."
 
 # Set environment variables
 SYSTEM_PYTHON=$(which python3)
 export CUDA_VISIBLE_DEVICES=0
 export OMP_NUM_THREADS=$LSB_DJOB_NUMPROC
 export MKL_NUM_THREADS=$LSB_DJOB_NUMPROC
-# export UV_CACHE_DIR="/scratch/$LSB_JOBID/.uv_cache"
 
 
 echo "Syncing environment..."
@@ -48,21 +39,18 @@ uv venv .venv --python "$SYSTEM_PYTHON"
 uv sync --python .venv/bin/python
 
 source .venv/bin/activate
-# Process documents with smaller chunk size to avoid memory issues
 echo ""
 echo "=========== training ================="
 
 python -m src.scripts.train_with_baseline \
     --on_cluster True \
-    --epochs 30 \
+    --epochs 100 \
     --learning_rate 1e-5 \
     --batch_size 10
-    # --data_dir "$LOCAL_DATA_DIR"
 
 EXIT_CODE=$?
 
-# echo "Cleaning up scratch..."
-# rm -rf "/scratch/$LSB_JOBID"
+
 
 echo "Job finished with exit code: $EXIT_CODE"
 exit $EXIT_CODE
