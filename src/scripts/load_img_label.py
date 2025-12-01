2a2842d71b58060b40229823f1f0279a7347fb63
Eliot Colas <eliot.colas29120@gmail.com>
2025-12-01 14:56:44 +0100

    baseline train

--- src/scripts/load_img_label.py
+++ src/scripts/load_img_label.py
@@ -1,5 +1,5 @@
-from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
 from torchvision.transforms import v2
 
 from ..utils import TOMODataset
@@ -9,20 +9,31 @@ label_dir = "datas/Original Masks"
 
 transform = v2.Compose(
     [
-        v2.RandomHorizontalFlip(p=0.5),
+        v2.RandomRotation([-20, 20]),
         v2.RandomVerticalFlip(p=0.5),
+        v2.RandomHorizontalFlip(p=0.5),
+        v2.RandomAffine(
+            degrees=180,
+            translate=(0.1, 0.1),
+            scale=(0.8, 1.2),
+            interpolation=v2.InterpolationMode.BILINEAR,
+        ),
+        v2.ElasticTransform(
+            alpha=50,
+            sigma=5,
+        ),
+        v2.GaussianBlur(kernel_size=(3, 7), sigma=(0.1, 2.0)),
+        v2.ColorJitter(brightness=0.2, contrast=0.2),
     ]
 )
 train_dataset = TOMODataset(img_dir, label_dir, transform=transform, split="train")
-test_dataset = TOMODataset(img_dir, label_dir, transform=None, split="test")
 
 train_dataloader = DataLoader(train_dataset, batch_size=4, shuffle=True)
-test_dataloader = DataLoader(test_dataset, batch_size=4, shuffle=False)
 
 for batch_idx, (images, labels, weights) in enumerate(train_dataloader):
     if batch_idx >= 3:
         break
-    fig, axes = plt.subplots(3, 4, figsize=(12, 9))
+    fig, axes = plt.subplots(2, 4, figsize=(12, 9), facecolor="#E3E3E3")
     for i in range(min(4, len(images))):
         # Image
         img = images[i].permute(1, 2, 0).numpy()
@@ -31,20 +42,20 @@ for batch_idx, (images, labels, weights) in enumerate(train_dataloader):
             axes[0, i].imshow(img, cmap="gray")
         else:
             axes[0, i].imshow(img)
-        axes[0, i].set_title(f"Image {i+1}")
+        # axes[0, i].set_title(f"Image {i + 1}")
         axes[0, i].axis("off")
 
         # Label
         label = labels[i].numpy()
         axes[1, i].imshow(label, cmap="gray")
-        axes[1, i].set_title(f"Mask {i+1}")
+        # axes[1, i].set_title(f"Mask {i + 1}")
         axes[1, i].axis("off")
 
-        # Weight
-        weight = weights[i].numpy()
-        axes[2, i].imshow(weight)
-        axes[2, i].set_title(f"weight {i+1}")
-        axes[2, i].axis("off")
+        # # Weight
+        # weight = weights[i].numpy()
+        # axes[2, i].imshow(weight)
+        # axes[2, i].set_title(f"weight {i + 1}")
+        # axes[2, i].axis("off")
     plt.tight_layout()
-    plt.savefig(f"src/scripts/train_batch_{batch_idx}.png")
+    plt.savefig(f"datas/datatransformEX/train_batch_{batch_idx}.png")
     plt.close()
